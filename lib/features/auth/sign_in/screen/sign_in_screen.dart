6517d2a8b5d43a1ac8a2ce670b06d5c2ee824a05
Jayrek <jrektabasa@gmail.com>
2025-02-07 17:29:15 +0800

    update sign in screen

--- lib/features/auth/sign_in/screen/sign_in_screen.dart
+++ lib/features/auth/sign_in/screen/sign_in_screen.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:todoapp/features/auth/sign_up/screen/sign_up_screen.dart';
+import 'package:todoapp/shared/widgets/custom_elevated_button_widget.dart';
+import 'package:todoapp/shared/widgets/custom_textformfield_widget.dart';
 
 class SignInScreen extends StatelessWidget {
   SignInScreen({super.key});
@@ -11,173 +14,123 @@ class SignInScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Form(
-      key: _formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
-            child: Align(
-              alignment: Alignment.bottomLeft,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Log In',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: -2,
-                      fontSize: 40,
-                    ),
-                  ),
-                  Text(
-                    'Please sign in to continue',
-                    style: TextStyle(fontSize: 14),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+        appBar: AppBar(),
+        body: SingleChildScrollView(
+          child: Form(
+            key: _formKey,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                TextFormField(
-                  validator: (value) {
-                    if (value != null && value.isEmpty) {
-                      return 'Please enter your email address';
-                    }
-                    return null;
-                  },
-                  controller: _emailController,
-                  decoration: InputDecoration(
-                    hintText: 'Email',
-                    prefixIcon: const Icon(
-                      Icons.email,
-                      color: Colors.grey,
-                    ),
-                    labelText: 'Email',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          const BorderSide(color: Colors.grey, width: 1),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          const BorderSide(color: Colors.blue, width: 1),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.red, width: 1),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.red, width: 1),
-                    ),
-                  ),
-                  maxLines: 1,
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  validator: (value) {
-                    if (value != null && value.isEmpty) {
-                      return 'Please enter your password';
-                    }
-                    return null;
-                  },
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    prefixIcon: const Icon(
-                      Icons.lock,
-                      color: Colors.grey,
-                    ),
-                    labelText: 'Password',
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          const BorderSide(color: Colors.grey, width: 1),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide:
-                          const BorderSide(color: Colors.blue, width: 1),
-                    ),
-                    errorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.red, width: 1),
-                    ),
-                    focusedErrorBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                      borderSide: const BorderSide(color: Colors.red, width: 1),
-                    ),
-                  ),
-                  maxLines: 1,
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20),
-            child: Row(
-              children: [
-                Expanded(
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: const Size(220, 60),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10),
+                const Padding(
+                  padding: EdgeInsets.fromLTRB(20, 0, 0, 20),
+                  child: Align(
+                    alignment: Alignment.bottomLeft,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Log In',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            letterSpacing: -2,
+                            fontSize: 40,
+                          ),
                         ),
-                        backgroundColor: Colors.black87),
-                    onPressed: () async {
-                      if (_formKey.currentState!.validate()) {
-                        _signInUser(
-                            _emailController.text, _passwordController.text);
-                      }
-                    },
-                    child: const Text(
-                      'LOGIN',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
+                        Text(
+                          'Please sign in to continue',
+                          style: TextStyle(fontSize: 14),
+                        ),
+                      ],
                     ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Column(
+                    children: [
+                      CustomTextFormFieldWidget(
+                        icon: Icons.email,
+                        controller: _emailController,
+                        hintText: 'Email',
+                        labelText: 'Email',
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return 'Please enter your email address';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      CustomTextFormFieldWidget(
+                        icon: Icons.lock,
+                        isObscure: true,
+                        controller: _passwordController,
+                        hintText: 'Password',
+                        labelText: 'Password',
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: CustomElevatedButtonWidget(
+                          backgroundColor: Colors.black87,
+                          labelText: 'LOGIN',
+                          onPressed: () async {
+                            if (_formKey.currentState!.validate()) {
+                              _signInUser(_emailController.text,
+                                  _passwordController.text);
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Don't have an account yet?",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SignUpScreen(),
+                            ));
+                      },
+                      child: const Text(
+                        'Register',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                    )
+                  ],
                 ),
               ],
             ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Don't have an account yet?",
-                style: TextStyle(fontSize: 14),
-              ),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Sign Up',
-                  style: TextStyle(fontSize: 14),
-                ),
-              )
-            ],
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 
   Future<void> _signInUser(String email, String password) async {
     try {
-      // final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
-      //   email: email,
-      //   password: password,
-      // );
-      debugPrint('credential: $email, $password');
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      debugPrint('credential: $credential');
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
       } else if (e.code == 'wrong-password') {
