78906e2bc05d2b3d2f0691453f839ccdf090b3dd
Jayrek <jrektabasa@gmail.com>
2025-02-15 19:33:28 +0800

    update TodoListScreen

--- lib/features/todo_list/screen/todo_list_screen.dart
+++ lib/features/todo_list/screen/todo_list_screen.dart
@@ -1,19 +1,53 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-class TodoListScreen extends StatelessWidget {
-  const TodoListScreen({
-    required this.ownerId,
-    super.key,
-  });
+class TodoListScreen extends StatefulWidget {
+  const TodoListScreen({super.key});
 
-  final String ownerId;
+  @override
+  State<TodoListScreen> createState() => _TodoListScreenState();
+}
+
+class _TodoListScreenState extends State<TodoListScreen> {
+  Map<String, dynamic>? _userData;
+
+  Future<void> getUserByDocID() async {
+    final String? userId = FirebaseAuth.instance.currentUser?.uid;
+    // if (userId == null) {
+    //   print("No user signed in");
+    //   return;
+    // }
+
+    DocumentReference userDoc =
+        FirebaseFirestore.instance.collection('user').doc(userId);
+
+    try {
+      DocumentSnapshot snapshot = await userDoc.get();
+
+      if (snapshot.exists) {
+        final userData = snapshot.data() as Map<String, dynamic>;
+        setState(() {
+          _userData = userData;
+        });
+        print("User found: ${userData['name']} - ${userData['email']}");
+      } else {
+        print("No user found with UID: $userId");
+      }
+    } catch (e) {
+      print("Error fetching user: $e");
+    }
+  }
 
   Stream<List<Map<String, dynamic>>> fetchUsersStream() {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    final userId = auth.currentUser?.uid;
+
     return FirebaseFirestore.instance
         .collection('todo')
-        .where('created_by', isEqualTo: ownerId)
+        .where('created_by', isEqualTo: userId)
+        .orderBy('created_date', descending: true)
         .snapshots()
         .map(
           (snapshot) => snapshot.docs
@@ -23,75 +57,102 @@ class TodoListScreen extends StatelessWidget {
   }
 
   @override
+  void initState() {
+    super.initState();
+    getUserByDocID();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('My ToDo List'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+            },
+            icon: const Icon(Icons.logout_outlined),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          Navigator.pushNamed(context, '/addTodo');
+        },
         backgroundColor: Colors.black87,
         child: const Icon(Icons.add, size: 30, color: Colors.white),
       ),
-      body: StreamBuilder<List<Map<String, dynamic>>>(
-        stream: fetchUsersStream(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
+      body: Column(
+        children: [
+          Text('Hi ${_userData?['name']}!'),
+          Expanded(
+            child: StreamBuilder<List<Map<String, dynamic>>>(
+              stream: fetchUsersStream(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-          if (snapshot.hasError) {
-            return Center(child: Text("Error: ${snapshot.error}"));
-          }
+                if (snapshot.hasError) {
+                  return Center(child: Text("Error: ${snapshot.error}"));
+                }
 
-          if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text("Your Todo LiSt is empty."));
-          }
+                if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text("Your Todo LiSt is empty."));
+                }
 
-          List<Map<String, dynamic>> todos = snapshot.data!;
+                List<Map<String, dynamic>> todos = snapshot.data!;
 
-          return ListView.builder(
-            itemCount: todos.length,
-            itemBuilder: (context, index) {
-              var todo = todos[index];
-              final isDone = todo['status'] == 0 ? true : false;
-              final timestamp = todo['created_date'];
-              final dateTime = timestamp.toDate();
-              final dateFormatted = DateFormat('MM/dd').format(dateTime);
-              return Card(
-                elevation: 4,
-                margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10),
-                  child: ListTile(
-                    title: Text(todo['name'],
-                        style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            decoration: isDone
-                                ? TextDecoration.lineThrough
-                                : TextDecoration.none)),
-                    subtitle: Text(todo['description'],
-                        style: TextStyle(
-                            fontSize: 20,
-                            decoration: isDone
-                                ? TextDecoration.lineThrough
-                                : TextDecoration.none)),
-                    trailing: Text(isDone ? '' : dateFormatted,
-                        style: const TextStyle(
-                          fontSize: 15,
-                          fontStyle: FontStyle.italic,
-                          fontWeight: FontWeight.bold,
-                        )),
-                  ),
-                ),
-              );
-            },
-          );
-        },
+                return ListView.builder(
+                  itemCount: todos.length,
+                  itemBuilder: (context, index) {
+                    var todo = todos[index];
+                    final isDone = todo['status'] == 0 ? true : false;
+                    final timestamp = todo['created_date'];
+                    final dateTime = timestamp.toDate();
+                    final dateFormatted = DateFormat('MM/dd').format(dateTime);
+                    return Card(
+                      elevation: 4,
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 5),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      color:
+                          isDone ? Colors.grey.shade500 : Colors.blue.shade100,
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: ListTile(
+                          title: Text(todo['name'],
+                              style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                  decoration: isDone
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none)),
+                          subtitle: Text(todo['description'],
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  decoration: isDone
+                                      ? TextDecoration.lineThrough
+                                      : TextDecoration.none)),
+                          trailing: Text(isDone ? '' : dateFormatted,
+                              style: const TextStyle(
+                                fontSize: 15,
+                                fontStyle: FontStyle.italic,
+                                fontWeight: FontWeight.bold,
+                              )),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
