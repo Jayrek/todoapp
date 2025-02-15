84605878684e8bb0cf05cb7666411e55ca6f24a3
Jayrek <jrektabasa@gmail.com>
2025-02-15 19:32:41 +0800

    add childWidget in CustomElevatedButtonWidget

--- lib/shared/widgets/custom_elevated_button_widget.dart
+++ lib/shared/widgets/custom_elevated_button_widget.dart
@@ -4,14 +4,14 @@ class CustomElevatedButtonWidget extends StatelessWidget {
   const CustomElevatedButtonWidget(
       {required this.backgroundColor,
       required this.onPressed,
-      required this.labelText,
+      required this.childWidget,
       this.textColor = Colors.white,
       super.key});
 
   final Color backgroundColor;
   final Function()? onPressed;
-  final String labelText;
   final Color textColor;
+  final Widget childWidget;
 
   @override
   Widget build(BuildContext context) {
@@ -22,13 +22,7 @@ class CustomElevatedButtonWidget extends StatelessWidget {
               RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
           backgroundColor: backgroundColor),
       onPressed: onPressed,
-      child: Text(
-        labelText,
-        style: TextStyle(
-          color: textColor,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+      child: childWidget,
     );
   }
 }
